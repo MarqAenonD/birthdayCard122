08e3169c953ae921cba2747680e8361ae08739b7
MarqAenonD <106565814+MarqAenonD@users.noreply.github.com>
2022-11-07 19:13:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,14 @@ function setup() {
     img = loadImage("https://images.unsplash.com/photo-1517273666229-35e76c06b18d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjY4MTc1MjU&ixlib=rb-4.0.3&q=80&w=400");
     video = createCapture(VIDEO);
     video.hide();
-
     tint_color = "";
 }
 
 function draw() {
     image(img, 0, 0, 640, 480)
     image(video, 450, 265, 140, 100);
+    circle(0, 55, 50)
+    eclipse(0, 55, 100, 50)
     tint(tint_color);
 }
 
